c0c175b20573c3e1ab284ceb5a3f020d5cffe6c6
shriram1056 <saravananshriram80@gmail.com>
2023-03-16 20:13:15 -0300

    pipe spawn functionality

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
 
-    public float moveSpeed = 2;
+    public float moveSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        transform.position +=  (Vector3.left * moveSpeed) * Time.deltaTime;
     }
 }
