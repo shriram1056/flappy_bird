884942d4a8afef45f52ce43602fb273361111f24
shriram1056 <saravananshriram80@gmail.com>
2023-09-17 15:22:26 -0300

    randomly spwan target in the screen

--- Assets/targetScript.cs
+++ Assets/targetScript.cs
@@ -6,11 +6,29 @@ public class targetScript : MonoBehaviour
 {
     public GameObject topPipe;
     public GameObject bottomPipe;
+    public float pipeOffset;
+    private RectTransform rectTransform;
+    private Camera mainCamera;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-       
+        // Get the RectTransform component of gameObject1
+        rectTransform = this.GetComponent<RectTransform>();
+        pipeOffset = this.transform.parent.gameObject.transform.position.y;
+
+        // Get the main camera
+        mainCamera = Camera.main;
+        // Check if we have a RectTransform and a main camera
+        if (rectTransform != null && mainCamera != null)
+        {
+           float minY = mainCamera.ViewportToWorldPoint(Vector3.zero).y + rectTransform.rect.height / 2f;
+            float maxY = mainCamera.ViewportToWorldPoint(Vector3.up).y - rectTransform.rect.height / 2f;
+            float randomY = Random.Range(minY, maxY);
+            // Set the position of gameObject1
+            rectTransform.position = new Vector3(rectTransform.position.x, randomY, 0f);
+
+            
+        }
     }
 
     // Update is called once per frame
@@ -24,6 +42,13 @@ public class targetScript : MonoBehaviour
         if(collision.gameObject.layer == 6){
             topPipe.transform.position += new Vector3(0,5,0);
             bottomPipe.transform.position += new Vector3(0,-6.6f,0);
+            
+            if(this.transform.localPosition.y > 0){
+                this.transform.position += new Vector3(0,5,0);
+            }else{
+            this.transform.position += new Vector3(0,-6.6f,0);
+            }
+
         }
     }
 }
